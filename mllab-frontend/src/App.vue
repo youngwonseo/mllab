07680f4f30c7eb4bf85579f5dc7694a7c5019270
young-won. Seo <jazz9008@gmail.com>
2018-10-04 18:18:48 +0900

    20181004

--- mllab-frontend/src/App.vue
+++ mllab-frontend/src/App.vue
@@ -1,11 +1,16 @@
 <template>
-  <div id="app">
-    <div class="side">
-      <a href="#">Dashboard</a>
-      <a href="#">Dashboard</a>
-    </div>
-    <router-view/>
-  </div>
+  <el-container>
+    <!-- <el-header>Header</el-header> -->
+    <el-container>
+      <el-aside width="200px">MLLAB</el-aside>
+      <el-container>
+        <el-main>
+          <router-view/>
+        </el-main>
+        <!-- <el-footer>Footer</el-footer> -->
+      </el-container>
+    </el-container>
+  </el-container>
 </template>
 
 <script>
@@ -20,36 +25,38 @@ body {
   margin: 0px;
   height: 100%;
   font-family: Helvetica, Verdana, sans-serif;
+  background-color:#202234;
 }
 
-.side{
-  top: 0;
-  left: 0;
-  height:100%;
-  width:150px;
-  float:left;
-}
-
-.side a {
-  padding: 5px 10px 5px 10px;
-  text-decoration: none;
-  display: block;
-}
-
-
-.container{
-  height: 100%;
-  display: flex;
-}
-
-#app{
-  height: 100%;
-}
-.left_div {
-  width:50%;
-}
-
-.right_div{
-  width:50%;
-}
+.el-header, .el-footer {
+    color: #fff;
+    text-align: center;
+    line-height: 60px;
+  }
+  
+  .el-aside {
+    color: #fff;
+    background-color:#31334a;
+    text-align: center;
+    line-height: 50px;
+  }
+  
+  .el-main {
+    padding: 0px;
+    color: #fff;
+    text-align: center;
+  }
+  
+  body > .el-container {
+    height: 100%;
+  }
+  
+  .el-container:nth-child(5) .el-aside,
+  .el-container:nth-child(6) .el-aside {
+    line-height: 260px;
+  }
+  
+  .el-container:nth-child(7) .el-aside {
+    line-height: 320px;
+  }
 </style>
--- mllab-frontend/src/components/DashboardPage.vue
+++ mllab-frontend/src/components/DashboardPage.vue
@@ -1,6 +1,6 @@
 <template>
   <div class="container">
-    <div class="left_div">
+    <div class="left-div">
       <l-map :zoom="zoom" :center="center">
         <l-tile-layer :url="url" :attribution="attribution"></l-tile-layer>
         <l-marker
@@ -15,10 +15,18 @@
         </l-marker>
       </l-map>
     </div>
-    <div class="right_div">
+    <div class="right-div">
+      <div class="right-top">
+        dsd
+      </div>
       <div class="list-group">
+
         <div v-for="device in getDevices" v-bind:key="device.id" class="list-item">
-          {{device.id}}
+          <div class="">
+            {{device.id}}, {{device.name}}, {{device.type}}, {{device.type}}
+          </div>
+          <div class=""></div>
+          <div class=""></div>
           <chart
             :options="device.data"
             :init-options="initOptions"
@@ -26,6 +34,7 @@
             theme="ovilia-green"
             auto-resize/>
         </div>
+
       </div>
     </div>
   </div>
@@ -81,6 +90,76 @@ export default {
           draggable: true,
           visible: true,
           data: this.getBar()
+        },
+        {
+          id: '111114',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스4',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111115',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스5',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111116',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스6',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111117',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스7',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111118',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스8',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111119',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스9',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
+        },
+        {
+          id: '111120',
+          position: {lat: 35.136151, lng: 129.101697},
+          name: '디바이스10',
+          type: 'Raspberry Pi',
+          tooltip: 'hi',
+          draggable: true,
+          visible: true,
+          data: this.getBar()
         }
       ]
     }
@@ -105,17 +184,42 @@ export default {
     },
     getBar () {
       return {
+        color: '#28BBAB',
+        grid: {
+          left: 30,
+          top: 30,
+          right: 30,
+          bottom: 30,
+          
+        },
         xAxis: {
-          type: 'category'
-          // data: []
+          type: 'category',
+          axisLabel: {
+            color: '#ffffff'
+          }
         },
         yAxis: {
-          type: 'value'
+          type: 'value',
+          axisLabel: {
+            color: '#ffffff'
+          }
         },
         series: [{
-          data: [0, 0, 0, 0, 0, 0, 0, 0, 0, 0],
+          data: [
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
+            0, 0, 0, 0, 0, 0, 0, 0, 0, 0
+          ],
           type: 'line',
-          smooth: false
+          smooth: false,
+          animation: false
         }]
       }
     },
@@ -126,34 +230,55 @@ export default {
     }
   },
   mounted () {
-    setInterval(()=>{
-      //console.log(this.devices)
+    setInterval(() => {
+      // console.log(this.devices)
       for (var i = 0, n = this.devices.length; i < n; i++) {
         for (var j = 0; j < 1; j++) {
           // console.log(this.devices[i].data.series[j].data.shift())
           this.devices[i].data.series[j].data.shift()
           this.devices[i].data.series[j].data.push(Math.random() * 700)
         }
-      }     
-    },500)
+      }
+    }, 500)
   }
 }
 </script>
 
 <style scoped>
+.container{
+  height: 100%;
+  display: flex;
+}
+
+.left-div {
+  width:50%;
+}
+
+.right-top{
+  position: absolute;
+  height: 150px;
+}
+
+.right-div{
+  height: 100%;
+  overflow-y: auto;
+  width: 50%;
+}
+
 .list-group {
-  height:100%;
-  overflow-y:auto;
+  margin-top: 150px;
   padding-left: 0;
 }
 
 .list-item {
+  
+  background-color: #31334a;
   margin: 10px;
   padding: 5px;
   border: 1px solid black;
 }
 
 .echarts {
-  height: 300px;
+  height: 200px;
 }
 </style>
